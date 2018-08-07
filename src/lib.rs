b3c2e09d69af980868d421bc7587a4af238e6b08
Henry de Valence <hdevalence@hdevalence.ca>
2018-08-07 04:09:10 -0700

    make Transcript public

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
 /// Transcript of a public coin argument
-struct Transcript {}
+pub struct Transcript {}
 
 impl Transcript {
     /// Initialize a new transcript with the supplied label.
-    fn new(label: &[u8]) -> Transcript {
+    pub fn new(label: &[u8]) -> Transcript {
         // Strobe init; meta-AD(label)
         unimplemented!();
     }
 
     /// Commit a prover's message to the transcript.
-    fn commit(&mut self, label: &[u8], message: &[u8]) {
+    pub fn commit(&mut self, label: &[u8], message: &[u8]) {
         // Strobe op: meta-AD(label || len(message)); AD(message)
         unimplemented!();
     }
 
     /// Fill the supplied buffer with the verifier's challenge bytes.
-    fn challenge(&mut self, label: &[u8], challenge_bytes: &mut [u8]) {
+    pub fn challenge(&mut self, label: &[u8], challenge_bytes: &mut [u8]) {
         // Strobe op: meta-PRF(label || len(challenge_bytes)); PRF into challenge_bytes
         unimplemented!();
     }
