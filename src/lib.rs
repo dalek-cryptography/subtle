792c385ef1f6c10019de0d8031dc2cbb5507605c
Mazdak Farrokhzad <twingoow@gmail.com>
2021-06-22 10:14:07 +0200

    drop `external_doc` feature gate usage

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,6 @@
 // - Henry de Valence <hdevalence@hdevalence.ca>
 
 #![no_std]
-#![cfg_attr(feature = "nightly", feature(external_doc))]
 #![cfg_attr(feature = "nightly", doc(include = "../README.md"))]
 #![cfg_attr(feature = "nightly", deny(missing_docs))]
 #![doc(html_logo_url = "https://doc.dalek.rs/assets/dalek-logo-clear.png")]
