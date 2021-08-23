01f8d483b5f732a60a031aa36125094304113394
Leonardo Razovic <leorazzo@gmail.com>
2021-08-23 08:41:33 +0200

    Update fuzzer

--- fuzz/fuzzers/conditional_assign_u8.rs
+++ fuzz/fuzzers/conditional_assign_u8.rs
@@ -1,10 +1,5 @@
 #![no_main]
-
-#[macro_use]
-extern crate libfuzzer_sys;
-extern crate subtle;
-extern crate core;
-
+use libfuzzer_sys::fuzz_target;
 use subtle::ConditionallySelectable;
 
 fuzz_target!(|data: &[u8]| {
