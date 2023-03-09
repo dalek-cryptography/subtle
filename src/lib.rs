415cb23f039e19e215abac3fa6d717b48b4bb25a
Amber Sprenkels <amber@electricdusk.com>
2023-03-09 20:39:00 +0800

    Revert "Change new black_box impl to also be #[inline(never)]."
    This reverts commit 6410953c200285f704556e3e3748e3e95a363532.

--- src/lib.rs
+++ src/lib.rs
@@ -243,7 +243,7 @@ fn black_box(input: u8) -> u8 {
 }
 
 #[cfg(feature = "core_hint_black_box")]
-#[inline(never)]
+#[inline]
 fn black_box(input: u8) -> u8 {
     debug_assert!((input == 0u8) | (input == 1u8));
     core::hint::black_box(input)
